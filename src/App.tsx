9553cc25ac49a46345d7ec51aaf4c97ca79a8c46
zoomdong <1344492820@qq.com>
2020-05-10 19:27:00 +0800

    feat: add Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com/" disabled>Baidu Link</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
