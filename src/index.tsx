5f4122c3defbc04589590d2b2abec07915a41a46
zoomdong <1344492820@qq.com>
2020-05-16 17:54:56 +0800

    refactor: index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import Button from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Fragment>
+      <Button className="custom">hello</Button>
+      <Button disabled>Disabled Button</Button>
+      <Button btnType="primary" size="lg">Large Primary Button</Button>
+      <Button btnType="danger" size="sm">Small Danger Button</Button>
+      <Button btnType="link" href="http://www.baidu.com" target="__blank">Link Button</Button>
+      <Button btnType="link" href="http://www.baidu.com" disabled>Disabled Link Button</Button>
+      <hr />
+      <Menu />
+      <MenuItem />
+    </Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
