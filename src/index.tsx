9209cc08d91046d4ed28269bd9441ecb60d878ca
zoomdong <1344492820@qq.com>
2020-05-16 17:29:48 +0800

    feat: files change

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss'
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
